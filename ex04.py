b136fa89060095df6bd7d4e5ae75039166d8b8d0
Rian dos Passos <riandpf2002@gmail.com>
2022-02-28 22:04:45 -0300

    Adionando exercicios

--- ex04.py
+++ ex04.py
@@ -2,13 +2,22 @@
 #unitário e o percentual de desconto a ser aplicado para o pagamento. Imprima na
 #tela o nome do produto e o valor total da venda.
 
-produto = 'Geladeira'
-quantidadeComprada = 2
-valorUnitario = 1200
-percentualDesconto = 5
+class total:
+    def __init__ (self):
+        self.produto = input("Insira o produto que você comprou: ")
+        self.quantidadeComprada = int(input("Insira a quantidade que você comprou: "))
+        self.valorUnitario = int(input("Qual o valor unitário do produto: "))
+        self.percentualDesconto = int(input("Qual o percentual de desconto: "))
 
-valorSemDesconto = valorUnitario * quantidadeComprada
-valorDesconto = (percentualDesconto / 100) * valorSemDesconto
-valorTotalVenda = valorSemDesconto - valorDesconto
+    def iniciar(self):
+        self.calculaValorVenda()
 
-print('Produto: ' + produto + ' Valor: ' + str(valorTotalVenda))
+    def calculaValorVenda(self):
+        valorSemDesconto = self.valorUnitario * self.quantidadeComprada
+        valorDesconto = (self.percentualDesconto / 100) * valorSemDesconto
+        valorTotalVenda = valorSemDesconto - valorDesconto
+
+        print('Produto: ' + self.produto + ' Valor total da venda: ' + str(valorTotalVenda))
+
+total = total()
+total.iniciar()
